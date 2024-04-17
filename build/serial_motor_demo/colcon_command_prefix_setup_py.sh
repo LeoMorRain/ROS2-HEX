528c1e262beb65d14622fe863daee40dd6c1bb48
LeoMorRain <soluckyme2024@163.com>
2024-04-17 20:05:16 +0800

    gazebo control

---
+++ build/serial_motor_demo/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/leomorningrain/code/sdk_mark2_mcnm_ws/install/serial_motor_demo_msgs/share/serial_motor_demo_msgs/package.sh"
---
+++ build/serial_motor_demo/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/leomorningrain/code/sdk_mark2_mcnm_ws/install/serial_motor_demo'
