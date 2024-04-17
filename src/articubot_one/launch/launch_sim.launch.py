071160d4810df6480462330a01d449b9c94e24d6
LeoMorRain <soluckyme2024@163.com>
2024-04-17 23:59:49 +0800

    gazebo control new

--- src/articubot_one/launch/launch_sim.launch.py
+++ src/articubot_one/launch/launch_sim.launch.py
@@ -79,17 +79,17 @@ def generate_launch_description():
         output="screen",
     )
 
-    # diff_drive_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner.py",
-    #     arguments=["diff_cont"],
-    # )
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
 
-    # joint_broad_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner.py",
-    #     arguments=["joint_broad"],
-    # )
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
     # Code for delaying a node (I haven't tested how effective it is)
     #
@@ -115,7 +115,7 @@ def generate_launch_description():
             # twist_mux,
             gazebo,
             spawn_entity,
-            # diff_drive_spawner,
-            # joint_broad_spawner,
+            diff_drive_spawner,
+            joint_broad_spawner,
         ]
     )
